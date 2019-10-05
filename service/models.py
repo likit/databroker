a58b699eb8c8e03bf44f680b4735930a7791f6ea
Likit Preeyanon <likit.pre@mahidol.edu>
2019-10-05 17:56:07 +0700

    added dataset and schema model
    added migration revision

--- service/models.py
+++ service/models.py
@@ -56,3 +56,30 @@ class OrgClientTeam(db.Model):
                         primary_key=True)
     org = db.relationship('OrgClient', backref=db.backref('team_assoc'))
     team = db.relationship('OrgTeam', backref=db.backref('org_assoc'))
+
+
+class Dataset(db.Model):
+    __tablename__ = 'dataset'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(), nullable=True)
+    fields = db.Column(db.JSON, nullable=False)
+    active = db.Column(db.Boolean(), default=True)
+    creator_id = db.Column(db.Integer, db.ForeignKey('org_person.id'))
+    creator = db.relationship('OrgPerson', backref=db.backref('datasets'))
+
+
+class DataSchema(db.Model):
+    __tablename__ = 'data_schema'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    created_at = db.Column(db.DateTime(timezone=True),
+                            server_default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True),
+                            onupdate=func.now())
+    schema = db.Column(db.JSON, nullable=False)
+    creator_id = db.Column(db.Integer,
+                            db.ForeignKey('org_person.id', ondelete='CASCADE'))
+    creator = db.relationship('OrgPerson', backref=db.backref('schemas'))
+    dataset_id = db.Column(db.Integer, db.ForeignKey('dataset.id'))
+    dataset = db.relationship('Dataset',
+                                backref=db.backref('schemas', lazy='dynamic'))
